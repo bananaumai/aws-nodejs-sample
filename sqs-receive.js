e0bb6511eaf65862f65eebd5aca9aae16d973fa6
Banana Shimakawa <shimakawa@c-fo.com>
2017-12-10 18:23:45 +0900

    add sqs sample

---
+++ sqs-receive.js
@@ -0,0 +1,39 @@
+var AWS = require('aws-sdk');
+
+AWS.config.update({region: process.env.AWS_REGION});
+
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+var queueURL = process.env.AWS_SQS_URL;
+
+var params = {
+  AttributeNames: [
+    "SentTimestamp"
+  ],
+  MaxNumberOfMessages: 1,
+  MessageAttributeNames: [
+    "All"
+  ],
+  QueueUrl: queueURL,
+  VisibilityTimeout: 0,
+  WaitTimeSeconds: 0
+};
+
+sqs.receiveMessage(params, function(err, data) {
+  if (err) {
+    console.log("Receive Error", err);
+  } else if (data.Messages) {
+    console.log(data.Messages);
+    var deleteParams = {
+      QueueUrl: queueURL,
+      ReceiptHandle: data.Messages[0].ReceiptHandle
+    };
+    sqs.deleteMessage(deleteParams, function(err, data) {
+      if (err) {
+        console.log("Delete Error", err);
+      } else {
+        console.log("Message Deleted", data);
+      }
+    });
+  }
+});
